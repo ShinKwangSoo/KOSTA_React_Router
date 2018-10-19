c901c7bac670697c4ec04fe8d5f6538bca0d2465
ksshin <sksbest4@gmail.com>
2018-10-19 16:46:32 +0900

    test

--- pages/Home.js
+++ pages/Home.js
@@ -1,12 +1,8 @@
 import React from 'react';
-
-const Home = ({ history }) => {
+const Home = () => {
     return (
         <div>
             <h2>홈</h2>
-            <button onClick={() => {
-                history.push('/about/javascript')
-            }}>자바스크립트를 사용하여 이동</button>
         </div>
     );
 };
